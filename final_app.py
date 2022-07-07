ee0ae585e6f08b211ad0ccf027b1ce8cf54d0a92
Raj Chhabria <rajchhabria@Rajs-MacBook-Air.local>
2022-07-07 11:03:37 +0530

    commit

--- final_app.py
+++ final_app.py
@@ -4,8 +4,15 @@ from fuzzywuzzy import process
 import pandas as pd
 import pdfplumber
 from PIL import Image
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import jellyfish
 
+stop_words = set(stopwords.words('english'))
 st.title("Resume Analyzer")
+filtered_sentence = ""
+resume_text = ""
 uploaded_file = None
 
 rad =st.sidebar.radio("Navigation",["About this app","Try it"])
@@ -26,8 +33,28 @@ if rad == "Try it":
               first_page = pdf.pages[0]
               resume_text = first_page.extract_text()
 
-  z = fuzz.token_set_ratio(skills, resume_text)
 
-
-  st.write("Match = ",z,"%")
+  word_tokens = word_tokenize(skills)
+  filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+  filtered_sentence = []
+  for w in word_tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+  def listToString(s):
+   
+    # initialize an empty string
+    str1 = ""
+   
+    # traverse in the string 
+    for ele in s:
+        str1 += ele 
+   
+    # return string 
+    return str1
+  ans = listToString(filtered_sentence)
+  #z = fuzz.token_set_ratio(filtered_sentence, resume_text)
+  z = jellyfish.jaro_distance(ans,resume_text)
+  z = z*100
+  output = round(z,2)
+  st.write("Match = ",output,"%")
 
